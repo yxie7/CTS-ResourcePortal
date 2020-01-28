cdd4c65577e34b498b1e58a191f5b7cc82e0074c
Ying Xie <n88yingxie@gmail.com>
2020-01-28 16:43:34 -0500

    All tables in NewsletterCreate autopopulates
    Also added resource id into all the model files

--- CTS-ResourcePortal/NewsletterCreate.aspx.designer.cs
+++ CTS-ResourcePortal/NewsletterCreate.aspx.designer.cs
@@ -31,6 +31,24 @@ namespace CTS_ResourcePortal {
         protected global::System.Web.UI.WebControls.GridView gvJob;
         
         /// <summary>
+        /// gvEvent control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView gvEvent;
+        
+        /// <summary>
+        /// gvTraining control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView gvTraining;
+        
+        /// <summary>
         /// btnPreview control.
         /// </summary>
         /// <remarks>

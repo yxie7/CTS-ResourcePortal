be2d5ff2bf90f9e491c967c9b97df0b85416ac8f
Ying Xie <n88yingxie@gmail.com>
2020-01-29 12:01:03 -0500

    data is passed between newsletter create and preview pages
    also removed unnecessary code from master pages

--- CTS-ResourcePortal/NewsletterPreview.aspx.cs
+++ CTS-ResourcePortal/NewsletterPreview.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Web.Script.Serialization;
 
 namespace CTS_ResourcePortal
 {
@@ -11,7 +12,16 @@ namespace CTS_ResourcePortal
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            Dictionary<string, string> selections = js.Deserialize<Dictionary<string, string>>(System.Text.Encoding.UTF8.GetString(Convert.FromBase64String(Request.QueryString["nl"])));
 
+            //<string,string> selections = js.Deserialize<Dictionary<string,string>>(Server.UrlDecode(Request.QueryString["nl"]));
+
+            foreach(KeyValuePair<string,string> selection in selections)
+            {
+                string id = selection.Key;
+                string comment = selection.Value;
+            }
         }
 
         protected void btnSend_Click(object sender, EventArgs e)

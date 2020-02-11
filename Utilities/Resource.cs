63132c0994e1057d1c07679ccdf772c3c4e63610
gabeconn26 <48735629+gabeconn26@users.noreply.github.com>
2020-02-10 21:25:16 -0500

    Built Resource class & Started SP for inactivating

---
+++ Utilities/Resource.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities
+{
+    public class Resource
+    {
+        private string ResourceID;
+        private int ResourceTypeID;
+        private int Active;
+
+        public string resourceID
+        {
+            get { return ResourceID; }
+            set { ResourceID = value; }
+        }
+
+        public int resourceTypeID
+
+        {
+            get { return ResourceTypeID; }
+            set { ResourceTypeID = value; }
+        }
+
+        public int active
+
+        {
+            get { return Active; }
+            set { Active = value; }
+        }
+
+    }
+}

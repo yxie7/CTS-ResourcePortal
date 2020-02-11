86f18102a0cef39406a0cbde60581304aca40ed8
DamienK1337 <54823632+DamienK1337@users.noreply.github.com>
2020-02-11 16:56:31 -0500

    Update ResourceList.aspx.cs

--- CTS-ResourcePortal/ResourceList.aspx.cs
+++ CTS-ResourcePortal/ResourceList.aspx.cs
@@ -33,18 +33,26 @@ namespace CTS_ResourcePortal
                     for (int i = 0; i < count; i++)
                     {
                         Job j = new Job();
+
                         j.resourceID = db.GetField("ResourcesID", i).ToString();
                         j.resourceTitle = db.GetField("ResourceName", i).ToString();
                         j.email = db.GetField("ContactEmail", i).ToString();
 
+                        if (!(String.IsNullOrEmpty(db.GetField("StartDate", i).ToString())))
+                        {
+                            var dateValue = db.GetField("StartDate", i);
+                            string date1 = Convert.ToDateTime(dateValue).ToString("MM-dd-yyyy");
+                            j.datePosted = Convert.ToDateTime(date1);
+                        }
 
-                        var dateValue = db.GetField("StartDate", i);
-                        string date1 = Convert.ToDateTime(dateValue).ToString("MM-dd-yyyy");
-                        j.datePosted = Convert.ToDateTime(date1);
+                        if (!(String.IsNullOrEmpty(db.GetField("EndDate", i).ToString())))
+                        {
+                            var dateValue2 = db.GetField("EndDate", i);
+                            string date2 = Convert.ToDateTime(dateValue2).ToString("MM-dd-yyyy");
+                            j.expDate = Convert.ToDateTime(date2);
+                        }
 
-                        var dateValue2 = db.GetField("EndDate", i);
-                        string date2 = Convert.ToDateTime(dateValue2).ToString("MM-dd-yyyy");
-                        j.expDate = Convert.ToDateTime(date2);
+                 
 
 
                         j.addressLine1 = db.GetField("AddressLine", i).ToString();
@@ -80,13 +88,20 @@ namespace CTS_ResourcePortal
                         E.email = db.GetField("ContactEmail", i).ToString();
 
 
-                        var dateValue = db.GetField("StartDate", i);
-                        string date1 = Convert.ToDateTime(dateValue).ToString("MM-dd-yyyy");
-                        E.eventDate = Convert.ToDateTime(date1);
+                        if (!(String.IsNullOrEmpty(db.GetField("StartDate", i).ToString())))
+                        {
+                            var dateValue = db.GetField("StartDate", i);
+                            string date1 = Convert.ToDateTime(dateValue).ToString("MM-dd-yyyy");
+                            E.eventDate = Convert.ToDateTime(date1);
+                        }
 
-                        var dateValue2 = db.GetField("EndDate", i);
-                        string date2 = Convert.ToDateTime(dateValue2).ToString("MM-dd-yyyy");
-                        E.expDate = Convert.ToDateTime(date2);
+                        if (!(String.IsNullOrEmpty(db.GetField("EndDate", i).ToString())))
+                        {
+                            var dateValue2 = db.GetField("EndDate", i);
+                            string date2 = Convert.ToDateTime(dateValue2).ToString("MM-dd-yyyy");
+                            E.expDate = Convert.ToDateTime(date2);
+                        }
+
 
 
                         E.addressLine1 = db.GetField("AddressLine", i).ToString();
@@ -120,13 +135,19 @@ namespace CTS_ResourcePortal
                         T.email = db.GetField("ContactEmail", i).ToString();
 
 
-                        var dateValue = db.GetField("StartDate", i);
-                        string date1 = Convert.ToDateTime(dateValue).ToString("MM-dd-yyyy");
-                        T.startDate = Convert.ToDateTime(date1);
+                        if (!(String.IsNullOrEmpty(db.GetField("StartDate", i).ToString())))
+                        {
+                            var dateValue = db.GetField("StartDate", i);
+                            string date1 = Convert.ToDateTime(dateValue).ToString("MM-dd-yyyy");
+                            T.startDate = Convert.ToDateTime(date1);
+                        }
 
-                        var dateValue2 = db.GetField("EndDate", i);
-                        string date2 = Convert.ToDateTime(dateValue2).ToString("MM-dd-yyyy");
-                        T.expDate = Convert.ToDateTime(date2);
+                        if (!(String.IsNullOrEmpty(db.GetField("EndDate", i).ToString())))
+                        {
+                            var dateValue2 = db.GetField("EndDate", i);
+                            string date2 = Convert.ToDateTime(dateValue2).ToString("MM-dd-yyyy");
+                            T.expDate = Convert.ToDateTime(date2);
+                        }
 
 
                         T.addressLine1 = db.GetField("AddressLine", i).ToString();
@@ -175,7 +196,7 @@ namespace CTS_ResourcePortal
             lblCompanyName.Text = (string)db.GetField("Company", 0);
             lblResponsibilities.Text = (string)db.GetField("Description", 0);
             lblHours.Text = (string)db.GetField("WeeklyHours", 0);
-            lblLength.Text = (string)db.GetField("Length", 0);
+            //lblLength.Text = (string)db.GetField("Length", 0);
 
             string addressLine1 = (string)db.GetField("AddressLine", 0);
             string city = (string)db.GetField("LocationCity", 0);
@@ -191,10 +212,10 @@ namespace CTS_ResourcePortal
             lblPosted.Text = Convert.ToDateTime(dateValue).ToString("MM-dd-yyyy");
 
 
-            var dateValue2 = db.GetField("EndDate", 0);
+            var dateValue2 = db.GetField("RegistrationDeadline", 0);
             lblExpired.Text = Convert.ToDateTime(dateValue2).ToString("MM-dd-yyyy");
 
-            lblAttire.Text = (string)db.GetField("Attire", 0);
+            //lblAttire.Text = (string)db.GetField("Attire", 0);
 
             lblOtherRequirements.Text = (string)db.GetField("Requirements", 0);
 
@@ -341,6 +362,7 @@ namespace CTS_ResourcePortal
             lblStartEnd.Visible = false;
             lblAttire.Visible = false;
             lblAttire1.Visible = false;
+            lblStartDt.Visible = false;
         }
         public void EventVisibilityControls()
         {

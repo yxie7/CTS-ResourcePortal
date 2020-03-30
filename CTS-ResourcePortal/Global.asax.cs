baa86b58ab176b7a28f63deed0af33c8cd13f935
Ying Xie <n88yingxie@gmail.com>
2020-03-30 19:06:32 -0400

    Added Global.asax and enforcing HTTPS

---
+++ CTS-ResourcePortal/Global.asax.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Web;
+
+namespace CTS_ResourcePortal
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            if (HttpContext.Current.Request.IsSecureConnection.Equals(false) && HttpContext.Current.Request.IsLocal.Equals(false))
+            {
+                Response.Redirect("https://" + Request.ServerVariables["HTTP_HOST"] + HttpContext.Current.Request.RawUrl);
+            }
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+        }
+    }
+}

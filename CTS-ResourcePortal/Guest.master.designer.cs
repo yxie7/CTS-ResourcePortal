10316405f217eedfb3c2265cc88d733e8dbf4cb9
Ying Xie <n88yingxie@gmail.com>
2020-03-31 00:04:20 -0400

    Removed old files and updated footer

--- CTS-ResourcePortal/Guest.master.designer.cs
+++ CTS-ResourcePortal/Guest.master.designer.cs
@@ -13,6 +13,15 @@ namespace CTS_ResourcePortal {
     public partial class Guest {
         
         /// <summary>
+        /// head control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
+        
+        /// <summary>
         /// Content control.
         /// </summary>
         /// <remarks>

d610c82e2a52c6a5bd7f7fd68cd02e247f48d597
yxie7 <45112538+yxie7@users.noreply.github.com>
2020-04-09 19:42:24 -0400

    Removed to making publishing work

--- CTS-ResourcePortal/Global.asax.cs
+++ CTS-ResourcePortal/Global.asax.cs
@@ -15,10 +15,10 @@ namespace CTS_ResourcePortal
 
         protected void Application_BeginRequest(object sender, EventArgs e)
         {
-            if (HttpContext.Current.Request.IsSecureConnection.Equals(false) && HttpContext.Current.Request.IsLocal.Equals(false))
-            {
-                Response.Redirect("https://" + Request.ServerVariables["HTTP_HOST"] + HttpContext.Current.Request.RawUrl);
-            }
+        //    if (HttpContext.Current.Request.IsSecureConnection.Equals(false) && HttpContext.Current.Request.IsLocal.Equals(false))
+        //    {
+        //        Response.Redirect("https://" + Request.ServerVariables["HTTP_HOST"] + HttpContext.Current.Request.RawUrl);
+        //    }
         }
 
         protected void Application_AuthenticateRequest(object sender, EventArgs e)

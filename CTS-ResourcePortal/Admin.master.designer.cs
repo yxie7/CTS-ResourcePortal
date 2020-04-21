7a9b8056c1e2120804493b4836c0dce60801ce79
yxie7 <45112538+yxie7@users.noreply.github.com>
2020-04-21 18:30:10 -0400

    navbar item for admin and removed hidden footer link

--- CTS-ResourcePortal/Admin.master.designer.cs
+++ CTS-ResourcePortal/Admin.master.designer.cs
@@ -33,15 +33,6 @@ namespace CTS_ResourcePortal
         protected global::System.Web.UI.WebControls.Label lblAdminName;
 
         /// <summary>
-        /// home control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlAnchor home;
-
-        /// <summary>
         /// lnkSingout control.
         /// </summary>
         /// <remarks>
--- CTS-ResourcePortal/Master.Master.designer.cs
+++ CTS-ResourcePortal/Master.Master.designer.cs
@@ -42,6 +42,15 @@ namespace CTS_ResourcePortal
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
+        /// adminHome control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlAnchor adminHome;
+
+        /// <summary>
         /// navli control.
         /// </summary>
         /// <remarks>
